b7d63aaa80db29bae101abf62851c040956ba9fd
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-11-30 13:09:15 +0330

    validation form

--- auth.js
+++ auth.js
@@ -1,19 +1,29 @@
 import { authHandeler } from "./utils/authorization.js"
 import { setCookie } from "./utils/cookie.js"
 import { postData } from "./utils/httpRequest.js"
+import validateForm from "./utils/validation.js"
 
 
 const usernameInput = document.getElementById("username")
 const passwordInput = document.getElementById("password")
 const submitButton = document.getElementById("login-button")
 
-const username = usernameInput.value
-const password = passwordInput.value
+
 
 const submitHandeler = async (event) =>{
+
+    const username = usernameInput.value
+    const password = passwordInput.value
+
+    const validation = validateForm(username, password)
+    if(!validation) return
+
     event.preventDefault()
+
     const response = await postData("auth/login",{username , password})
+
     setCookie(response.token)
+    
     location.assign("index.html")
 } 
 
