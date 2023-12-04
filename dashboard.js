8641c0e6cf67c1050d610b255f1f24fe132c4235
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-12-04 10:32:45 +0330

    get users and show them

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,44 @@
 import { authHandeler } from "./utils/authorization.js";
+import { getData } from "./utils/httpRequest.js";
 
-const initHandler = () => {
-    authHandeler()
+const container = document.querySelector(".container")
+
+const renderUsers = (users) => {
+    
+    container.innerHTML = "";
+
+    users.map(user => {
+        const JSX = `<div id="card">
+                        <h3>${user.id}</h3>
+                        <div>
+                            <p><i class="fa-solid fa-user"></i> Name:</p>
+                            <span>${user.name.firstname} ${user.name.lastname}</span>
+                        </div>
+                        <div>
+                            <p><i class="fa-solid fa-paperclip"></i> UserName:</p>
+                            <span>${user.username}</span>
+                        </div>
+                        <div>
+                            <p><i class="fa-solid fa-envelope"></i> Email:</p>
+                            <span>${user.email}</span>
+                        </div>
+                        <div>
+                            <p><i class="fa-solid fa-phone"></i> Phone:</p>
+                            <span>${user.phone}</span>
+                        </div>
+                        <div>
+                            <p><i class="fa-solid fa-location-dot"></i> Address:</p>
+                        <span>${user.address.city}-${user.address.street}-${user.address.zipcode}</span>
+                    </div>
+        
+                    </div>`
+        container.innerHTML += JSX            
+    })
 }
 
+const initHandler = async() => {
+    authHandeler()
+    const users = await getData("users")
+    renderUsers(users)
+}
 document.addEventListener("DOMContentLoaded",initHandler);
